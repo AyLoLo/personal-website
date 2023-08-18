b54a3132565a5e53877df680af2ec462559c6380
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-17 22:10:55 -0400

    span text to highlight key words in About Me

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
       <div className="h-96 ml-60 relative">
         <h6 className='text-white text-7xl p-5'>Welcome.</h6>
         <p className="text-white w-6/12 p-5 absolute left-px  border-yellow-400 border-l-2">
-          My name is Anthony Lopez and I am a full-stack application developer based in Flushing, New York, US.  
-          As a graduate from Flatiron School, I am experienced in Python with Flask, Javascript, React, SQL Databases and RESTful API architecture based-programming. 
-          I also possess strong skills in community building and communication that help businesses and private organizations cultivate growth and empower supporting communities.
+          My name is <span className='text-blue-400 text-xl'>Anthony Lopez</span> and I am a <span className='text-blue-400 text-xl'>full-stack application developer</span> based in Flushing, New York, US.  
+          As a graduate from Flatiron School, I am experienced in <span className='text-blue-400 text-xl'>Python with Flask, Javascript, React, SQL Databases and RESTful API</span> architecture based-programming. 
+          I also possess strong skills in <span className='text-blue-400 text-xl'>community building</span> and communication that help businesses and private organizations cultivate growth and empower supporting communities.
             <br></br>
             <br></br>
-          My passions are to utilize the fundamental knowledge of software design and development to deploy full-stack websites, focusing on ease of use and efficiency.
+          My passions are to utilize the fundamental knowledge of software design and development to deploy full-stack websites, <span className='text-blue-400 text-xl'> focusing on ease of use and efficiency</span>.
         </p>
       </div>
     </div>
