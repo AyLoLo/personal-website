ae4618f49ef498d6341b379b0f6abc6d338c7604
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-17 22:51:05 -0400

    Included picture of myself, need to revisit scaling issue with screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import Me from './assets/Me.jpg';
 
 function App() {
   return (
@@ -6,16 +7,18 @@ function App() {
       <div className='text-white m-12 w-3/12 h-40 border-l-4 border-t-4 border-blue-400'>
         Space Holder?
       </div> 
-      <div className="h-96 ml-60 relative">
+      <div className="ml-60 relative">
         <h6 className='text-white text-7xl p-5'>Welcome.</h6>
-        <p className="text-white w-6/12 p-5 absolute left-px  border-yellow-400 border-l-2">
+        <p className="text-white w-7/12 p-5 absolute left-px  border-yellow-400 border-l-2">
           My name is <span className='text-blue-400 text-xl'>Anthony Lopez</span> and I am a <span className='text-blue-400 text-xl'>full-stack application developer</span> based in Flushing, New York, US.  
           As a graduate from Flatiron School, I am experienced in <span className='text-blue-400 text-xl'>Python with Flask, Javascript, React, SQL Databases and RESTful API</span> architecture based-programming. 
           I also possess strong skills in <span className='text-blue-400 text-xl'>community building</span> and communication that help businesses and private organizations cultivate growth and empower supporting communities.
             <br></br>
             <br></br>
-          My passions are to utilize the fundamental knowledge of software design and development to deploy full-stack websites, <span className='text-blue-400 text-xl'> focusing on ease of use and efficiency</span>.
+            <br></br>
+          My passion is to utilize the fundamental knowledge of software design and development to deploy full-stack websites, <br></br> <span className='text-blue-400 text-xl'> focusing on ease of use and efficiency</span>.
         </p>
+        <img className="absolute top-px right-px max-w-md mr-40 w-full" src={Me}/> 
       </div>
     </div>
   );
