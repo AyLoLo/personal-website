ee98c9dc4804aaa9692620bfb541026a41d22b8f
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-17 23:54:27 -0400

    Included logos to present social links, need to add onClick functions for each

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './index.css';
 import Me from './assets/Me.jpg';
+import Gmail from './assets/Gmail.png';
+import Github from './assets/Github.png';
+import Dev from './assets/Dev.png'
 
 function App() {
   return (
     <div>
-      <div className='text-white m-12 w-3/12 h-40 border-l-4 border-t-4 border-blue-400'>
-        Space Holder?
+      <div className='grid grid-cols-2'>
+        <div className='text-white text-4xl font-mono m-12 w-7/12 h-3/4 border-l-4 border-t-4 border-blue-400 relative p-10'>
+          @AyLolo
+        </div>
+        <div className='m-20 w-6/12  grid grid-cols-3 grid-rows-1 justify-center items-center'>
+          <img className="w-20" src={Gmail}/>
+          <img className="w-12" src={Github}/>
+          <img className='w-12' src={Dev}/>
+        </div>
       </div> 
       <div className="ml-60 relative">
-        <h6 className='text-white text-7xl p-5'>Welcome.</h6>
+        <h6 className='text-white font-medium text-7xl p-5'>Welcome.</h6>
         <p className="text-white w-7/12 p-5 absolute left-px  border-yellow-400 border-l-2">
           My name is <span className='text-blue-400 text-xl'>Anthony Lopez</span> and I am a <span className='text-blue-400 text-xl'>full-stack application developer</span> based in Flushing, New York, US.  
           As a graduate from Flatiron School, I am experienced in <span className='text-blue-400 text-xl'>Python with Flask, Javascript, React, SQL Databases and RESTful API</span> architecture based-programming. 
@@ -18,7 +28,7 @@ function App() {
             <br></br>
           My passion is to utilize the fundamental knowledge of software design and development to deploy full-stack websites, <br></br> <span className='text-blue-400 text-xl'> focusing on ease of use and efficiency</span>.
         </p>
-        <img className="absolute top-px right-px max-w-md mr-40 w-full" src={Me}/> 
+        <img className="absolute top-px right-px max-w-md mr-40 w-full rounded-md" src={Me}/> 
       </div>
     </div>
   );
