080b61ccc63a14bda929e89e5d49cf2501ba8524
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-17 22:01:30 -0400

    About Me section placed correctly with text content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import './index.css';
 
 function App() {
   return (
-    <div className="h-96 m-10 flex">
-      <h6 className='text-white text-7xl p-5  border-t-4 border-l-4 border-yellow-400'>Welcome.</h6>
+    <div>
+      <div className='text-white m-12 w-3/12 h-40 border-l-4 border-t-4 border-blue-400'>
+        Space Holder?
+      </div> 
+      <div className="h-96 ml-60 relative">
+        <h6 className='text-white text-7xl p-5'>Welcome.</h6>
+        <p className="text-white w-6/12 p-5 absolute left-px  border-yellow-400 border-l-2">
+          My name is Anthony Lopez and I am a full-stack application developer based in Flushing, New York, US.  
+          As a graduate from Flatiron School, I am experienced in Python with Flask, Javascript, React, SQL Databases and RESTful API architecture based-programming. 
+          I also possess strong skills in community building and communication that help businesses and private organizations cultivate growth and empower supporting communities.
+            <br></br>
+            <br></br>
+          My passions are to utilize the fundamental knowledge of software design and development to deploy full-stack websites, focusing on ease of use and efficiency.
+        </p>
+      </div>
     </div>
   );
 }
