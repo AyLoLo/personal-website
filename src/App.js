522136b15f9cc6824bb16f3af0ab698e1499838a
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-21 13:35:55 -0400

    Added HREF to LI elements Project PokeGallery and Octopath Hub, directs you to respective Github Repos

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='grid grid-rows-3 h-screen'>
       <div className='grid grid-cols-2'>
-        <div className='text-white text-4xl font-mono m-12 w-7/12 h-3/4 border-l-4 border-t-4 border-blue-400 relative p-10'>
+        <div className='text-white text-4xl text-opacity-80 font-mono m-12 w-7/12 h-3/4 border-l-4 border-t-4 border-blue-400 relative p-10'>
           @AyLolo
         </div>
         <div className='mb-16 mr-20 w-6/12  grid grid-cols-3 grid-rows-1 justify-center items-center'>
@@ -26,25 +26,31 @@ function App() {
       </div> 
       <div className="ml-40 relative">
         <h6 className='text-white font-medium text-7xl pl-5 pb-5'>Welcome.</h6>
-        <p className="text-white w-7/12 p-4 absolute left-px  border-yellow-400 border-l-2">
-          My name is <span className='text-blue-400 text-xl'>Anthony Lopez</span> and I am a <span className='text-blue-400 text-xl'>full-stack application developer</span> based in Flushing, New York, US.  
-          As a graduate from Flatiron School, I am experienced in <span className='text-blue-400 text-xl'>Python with Flask, Javascript, React, SQL Databases and RESTful API</span> architecture based-programming. 
-          I also possess strong skills in <span className='text-blue-400 text-xl'>community building</span> and communication that help businesses and private organizations cultivate growth and empower supporting communities.
+        <p className="text-white w-7/12 p-4 border-yellow-400 border-l-2">
+          My name is <span className='text-blue-400 text-2xl'>Anthony Lopez</span> and I am a <span className='text-blue-400 text-2xl'>full-stack application developer</span> based in Flushing, New York, US.  
+          As a graduate from Flatiron School, I am experienced in <span className='text-blue-400 text-2xl'>Python with Flask, Javascript, React, SQL Databases and RESTful API</span> architecture based-programming. 
+          I also possess strong skills in <span className='text-blue-400 text-2xl'>community building</span> and communication that help businesses and private organizations cultivate growth and empower supporting communities.
             <br></br>
             <br></br>
             <br></br>
           My passion is to utilize the fundamental knowledge of software design and development to deploy full-stack websites, <br></br> <span className='text-blue-400 text-xl'> focusing on ease of use and efficiency</span>.
         </p>
-        <img className="absolute top-px right-px max-w-md mr-40 w-full rounded-lg" alt="Anthony Lopez" src={Me}/> 
+        <img className="absolute top-px right-px  w-1/4 mr-40 rounded-lg" alt="Anthony Lopez" src={Me}/> 
       </div>
       <div className='grid grid-cols-3 w-screen pt-20'>
-        <div className='text-white text-4xl font-mono text-center pt-20 ml-60'>
+        <div className='text-white text-4xl text-opacity-80 font-mono text-center pt-20 ml-60'>
           Projects
         </div>
-        <div className='text-white text-xl pt-5 pl-16 border-yellow-400 border-l-4 mb-10'>
-          <li>Thought Scroller</li><br></br>
-          <li>PokeGallery</li><br></br>
-          <li>Octopath Hub </li>
+        <div className='text-white font-serif text-xl pt-5 pl-16 border-yellow-400 border-l-4 mb-10'>
+          <br></br>
+          <a href="https://github.com/AyLoLo/PokeGallery" target="_blank" rel="noreferrer">
+            <li>PokeGallery</li>
+          </a>
+          <br></br>
+          <br></br>
+          <a href="https://github.com/AyLoLo/Octopath-Hub" target="_blank" rel="noreferrer">
+            <li>Octopath Hub</li>
+          </a>
         </div>
         <div className='border-blue-400 border-r-4 border-b-4 mb-10 mr-10'>
         </div>
